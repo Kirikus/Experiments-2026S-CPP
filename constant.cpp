efaee7957923fd846f85e15cff7368541e5ea7a9
Denis1325 <penkovdenis271@gmail.com>
2026-03-13 14:15:30 +0300

    made getters non-constant, remove repeated lines in constant

--- constant.cpp
+++ constant.cpp
@@ -5,13 +5,13 @@ value(value), error(error), name(name), readonly(readonly) {
 
 }
 
-double Constant::get_value() const {
+double Constant::get_value() {
     return value;
 }
-double Constant::get_error() const {
+double Constant::get_error() {
     return error;
 }
-QString Constant::get_name() const {
+QString Constant::get_name() {
     return name;
 }
 
@@ -30,5 +30,3 @@ void Constant::set_error(double& new_error) {
 void Constant::set_name(QString& new_name) {
     name = new_name;
 }
-void set_error();
-void set_name();

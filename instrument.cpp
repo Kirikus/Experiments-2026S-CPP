fb67ffd2091f5c96b8708b68679a64bfaeade1ab
Denis1325 <penkovdenis271@gmail.com>
2026-03-16 14:25:29 +0300

    implemented instrument's constructor

--- instrument.cpp
+++ instrument.cpp
@@ -1,5 +1,7 @@
 #include "instrument.h"
 
+Instrument::Instrument(const QString& name, double error_value) : name(name), error_value(error_value) {}
+
 double Instrument::get_error_value() const {
     return error_value;
 }

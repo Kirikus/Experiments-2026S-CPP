bdb8bbc217852e95c35f5fe1d6d4a8a570c619ea
Denis1325 <penkovdenis271@gmail.com>
2026-03-16 15:45:12 +0300

    added the InstrumentTableModel class

---
+++ instrument_table_model.cpp
@@ -0,0 +1,32 @@
+#include "instrument_table_model.h"
+
+#include "Qt"
+
+InstrumentTableModel::InstrumentTableModel(Experiment& exp) : experiment(exp) {}
+
+int InstrumentTableModel::getRowCount() {
+    return rowCount;
+}
+
+int InstrumentTableModel::getColumnCount() {
+    return columnCount;
+}
+
+const Instrument& InstrumentTableModel::data(int role, int i) const { 
+    if (role == Qt::DisplayRole) {
+        return experiment.getInstrument(i);
+    }
+}
+
+void InstrumentTableModel::setData(int i, auto new_data, int data_type) {
+    Instrument& instrument = experiment.getInstrument(i);
+    if (data_type == 0) {
+        instrument.set_name(new_data);
+    } else if (data_type == 1) {
+        instrument.set_error_value(new_data);
+    } else if (data_type == 2) {
+        instrument.set_error_type(new_data);
+    } else {
+        throw std::invalid_argument("data_type must be 0 to 2");
+    }
+}
---
+++ instrument_table_model.h
@@ -0,0 +1,20 @@
+#ifndef INSTRUMENT_TABLE_MODEL_H
+#define INSTRUMENT_TABLE_MODEL_H
+
+#include "experiment.h"
+
+class InstrumentTableModel {
+public:
+  InstrumentTableModel(Experiment& exp);
+  int getRowCount();
+  int getColumnCount();
+  const Instrument& data(int role, int i) const;
+  void setData(int i, auto new_data, int data_type); // int data_type: 0 - name, 1 - error_value, 2 - error_type
+
+private:
+  int rowCount;
+  int columnCount;
+  Experiment& experiment;
+};
+
+#endif // INSTRUMENT_TABLE_MODEL

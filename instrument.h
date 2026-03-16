8e4d53f3cd78738f4c76058d2c22a74b0a00ff99
Denis1325 <penkovdenis271@gmail.com>
2026-03-16 15:44:26 +0300

    change wrong names in the Instrument

--- instrument.h
+++ instrument.h
@@ -11,7 +11,7 @@ public:
   const QString& get_error_type() const;
   void set_error_value(double new_error_value);
   void set_name(QString& new_name);
-  void get_error_type(QString& new_error_type);
+  void set_error_type(QString& new_error_type);
 
 protected:
   QString name;        //   название инструмента

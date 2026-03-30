341c69a99598a6ddf6283333295846cb3219cf68
Taisia <taisia0106@gmail.com>
2026-03-30 14:57:27 +0300

    added constant table model

---
+++ constant_table_model.cpp
@@ -0,0 +1,105 @@
+#include "constant_table_model.h"
+
+#include "Qt"
+
+ConstantTableModel::ConstantTableModel()
+    : rows(0), columns(3)
+{
+}
+
+int ConstantTableModel::rowCount() const
+{
+    return rows;
+}
+
+int ConstantTableModel::columnCount() const
+{
+    return columns;
+}
+
+QVariant ConstantTableModel::data(const QModelIndex &index, int role) const
+{
+    int row = index.row();
+    int col = index.column();
+
+    if (role == Qt::DisplayRole)
+    {
+        const Constant &constant = constants[index.row()];
+
+        switch (index.column())
+        {
+        case 0:
+            return constant.get_name();
+        case 1:
+            return constant.get_value();
+        case 2:
+            return constant.get_meaning();
+        }
+    }
+    return QVariant();
+}
+
+bool ConstantTableModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    if (role == Qt::EditRole)
+    {
+        if (!checkIndex(index))
+            return false;
+
+        Constant &constant = constants[index.row()];
+
+        switch (index.column())
+        {
+        case 0:
+            constant.set_name(value.toString());
+            break;
+        case 1:
+            constant.set_value(value.toDouble());
+            break;
+        case 2:
+            constant.set_meaning(value.toString());
+            break;
+        }
+
+        return true;
+    }
+    return false;
+}
+
+QVariant ConstantTableModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal)
+    {
+        switch (section)
+        {
+        case 0:
+            return QString("Имя");
+        case 1:
+            return QString("Значение");
+        case 2:
+            return QString("Обозначение");
+        }
+    }
+    return QVariant();
+}
+
+void ConstantTableModel::setHeader(QString &new_header)
+{
+    header = new_header;
+}
+
+Qt::ItemFlags ConstantTableModel::flags(const QModelIndex &index) const
+{
+    return Qt::ItemIsEditable | QAbstractTableModel::flags(index);
+}
+
+void ConstantTableModel::addConstant()
+{
+    beginInsertRows(QModelIndex(), rows, rows);
+
+    constants.append(Constant("", 0.0, "", false));
+
+    rows++;
+
+    endInsertRows();
+}

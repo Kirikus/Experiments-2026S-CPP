656f1f83ad12e22c6c8eb3984e950be9ac20317a
Denis1325 <penkovdenis271@gmail.com>
2026-03-30 15:22:54 +0300

    added delegates for variables and instruments

---
+++ variable_delegate.h
@@ -0,0 +1,28 @@
+#ifndef VARIABLE_DELEGATE_H
+#define VARIABLE_DELEGATE_H
+
+#include <QStyledItemDelegate>
+
+class VariableDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+
+public:
+    explicit VariableDelegate(QObject *parent = nullptr);
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &option,
+               const QModelIndex &index) const override;
+
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
+                         const QModelIndex &index) const override;
+
+    void setEditorData(QWidget *editor, const QModelIndex &index) const override;
+
+    void setModelData(QWidget *editor, QAbstractItemModel *model,
+                      const QModelIndex &index) const override;
+
+    void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option,
+                             const QModelIndex &index) const override;
+};
+
+#endif // VARIABLE_DELEGATE_H

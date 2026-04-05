4724f14b8103a734839163a202100298751bae46
Taisia <taisia0106@gmail.com>
2026-04-06 00:37:33 +0300

    add constant delegate

---
+++ constant_delegate.cpp
@@ -0,0 +1,80 @@
+
+#include "constant_delegate.h"
+
+#include <QPainter>
+#include <QLineEdit>
+#include <QDoubleSpinBox>
+
+ConstantDelegate::ConstantDelegate(QObject *parent)
+    : QStyledItemDelegate(parent)
+{
+}
+
+void ConstantDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
+                             const QModelIndex &index) const
+{
+    QStyledItemDelegate::paint(painter, option, index);
+}
+
+QWidget *ConstantDelegate::createEditor(QWidget *parent,
+                                        const QStyleOptionViewItem &option,
+                                        const QModelIndex &index) const
+{
+    int column = index.column();
+
+    // Колонка 0 — имя (текст)
+    if (column == 0) {
+        QLineEdit *editor = new QLineEdit(parent);
+        return editor;
+    }
+    // Колонка 1 — значение (число)
+    else if (column == 1) {
+        QDoubleSpinBox *editor = new QDoubleSpinBox(parent);
+        editor->setMinimum(-999999.0);
+        editor->setMaximum(999999.0);
+        editor->setDecimals(6);
+        editor->setSingleStep(0.1);
+        return editor;
+    }
+    // Колонка 2 — обозначение (текст)
+    else if (column == 2) {
+        QLineEdit *editor = new QLineEdit(parent);
+        return editor;
+    }
+
+    return QStyledItemDelegate::createEditor(parent, option, index);
+}
+
+void ConstantDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
+{
+    QVariant data = index.model()->data(index, Qt::EditRole);
+
+    QLineEdit *lineEdit = qobject_cast<QLineEdit*>(editor);
+    QDoubleSpinBox *spinBox = qobject_cast<QDoubleSpinBox*>(editor);
+
+    if (lineEdit) {
+        lineEdit->setText(data.toString());
+    } else if (spinBox) {
+        spinBox->setValue(data.toDouble());
+    }
+}
+
+void ConstantDelegate::setModelData(QWidget *editor, QAbstractItemModel *model,
+                                    const QModelIndex &index) const
+{
+    QLineEdit *lineEdit = qobject_cast<QLineEdit*>(editor);
+    QDoubleSpinBox *spinBox = qobject_cast<QDoubleSpinBox*>(editor);
+
+    if (lineEdit) {
+        model->setData(index, lineEdit->text(), Qt::EditRole);
+    } else if (spinBox) {
+        model->setData(index, spinBox->value(), Qt::EditRole);
+    }
+}
+
+void ConstantDelegate::updateEditorGeometry(QWidget *editor,
+                                            const QStyleOptionViewItem &option,
+                                            const QModelIndex &index) const
+{
+    editor->setGeometry(option.rect);
+}

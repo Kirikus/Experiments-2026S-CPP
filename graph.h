537d2a1188a2e6e19c07d95a8d1337c9e9b8d89a
Taisia <taisia0106@gmail.com>
2026-04-06 16:05:05 +0300

    changed graph class

--- graph.h
+++ graph.h
@@ -2,9 +2,13 @@
 #define GRAPH_H
 
 #include <QWidget>
+#include <QPushButton>
 
-namespace Ui {
-class Graph;
+class QCustomPlot;
+
+namespace Ui
+{
+    class Graph;
 }
 
 class Graph : public QWidget
@@ -15,7 +19,20 @@ public:
     explicit Graph(QWidget *parent = nullptr);
     ~Graph();
 
+    enum GraphType
+    {
+        Line,
+        Bar,
+        Colour
+    };
+
+    void setGraphType(GraphType type);
+
 private:
+    QCustomPlot *m_plot;
+    QPushButton *m_settingsButton;
+    GraphType m_type;
+
     Ui::Graph *ui;
 };
 

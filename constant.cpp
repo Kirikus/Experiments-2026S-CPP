09bcbed0f8bab52846b4bc271d8f4be88f72162b
Taisia <taisia0106@gmail.com>
2026-04-11 17:41:35 +0300

    ﻿Update constant class

--- constant.cpp
+++ constant.cpp
@@ -1,7 +1,7 @@
 #include "constant.h"
 
-Constant::Constant(double& value, double& error, QString& name, bool readonly) :
-value(value), error(error), name(name), readonly(readonly) {
+Constant::Constant(const QString& name, double value, double error, const QString& meaning, bool readonly) :
+name(name), value(value), error(error), meaning(meaning), readonly(readonly) {
 
 }
 
@@ -11,6 +11,33 @@ double Constant::get_value() const {
 double Constant::get_error() const {
     return error;
 }
+QString Constant::get_meaning() const {
+    return meaning;
+}
 QString Constant::get_name() const {
     return name;
+}
+
+void Constant::set_value(double new_value) {
+    if (!readonly) {
+        value = new_value;
+    }
+}
+
+void Constant::set_error(double new_error) {
+    if (!readonly) {
+        error = new_error;
+    }
+}
+
+void Constant::set_meaning(const QString &new_meaning) {
+    if (!readonly) {
+        meaning = new_meaning;
+    }
+}
+
+void Constant::set_name(const QString &new_name) {
+    if (!readonly) {
+    name = new_name;
+    }
 }

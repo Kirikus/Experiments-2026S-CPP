c3b62d2be98c94cac5b9796632f62e6fc46ae9e4
Taisia <taisia0106@gmail.com>
2026-04-11 17:40:17 +0300

    Add constant table model

---
+++ constant_table_model.h
@@ -0,0 +1,30 @@
+#ifndef CONSTANT_TABLE_MODEL_H
+#define CONSTANT_TABLE_MODEL_H
+
+#include "QModelIndex"
+#include "Qt"
+#include "constant.h"
+
+class ConstantTableModel : public QAbstractTableModel
+{
+public:
+    explicit ConstantTableModel();
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    void setHeader(QString& new_header);
+
+    void addConstant();
+
+private:
+    int columns;
+    QList<Constant> constants;
+    QString header;
+};
+
+#endif // CONSTANT_TABLE_MODEL_H

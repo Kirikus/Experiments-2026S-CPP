ed6413e904f412a2a3cfdfb99b74ba45c1db5ee6
Taisia <taisia0106@gmail.com>
2026-05-10 19:42:33 +0300

    graph block

--- graphblock.cpp
+++ graphblock.cpp
@@ -1,14 +1,29 @@
 #include "graphblock.h"
 #include "ui_graphblock.h"
+#include <qcustomplot.h>
 
 GraphBlock::GraphBlock(QWidget *parent)
-    : QDialog(parent)
+    : Block(parent)
     , ui(new Ui::GraphBlock)
 {
     ui->setupUi(this);
+
+    connect(ui->upButton, &QPushButton::clicked, this, &Block::moveUpButton);
+    connect(ui->downButton, &QPushButton::clicked, this, &Block::moveDownButton);
+    connect(ui->deleteButton, &QPushButton::clicked, this, &Block::deleteButton);
 }
 
 GraphBlock::~GraphBlock()
 {
     delete ui;
 }
+
+//отрисовка картинки графика
+void GraphBlock::setPlot(QCustomPlot *sourcePlot)
+{
+    if (sourcePlot) {
+        QPixmap pixmap = sourcePlot->toPixmap();
+        ui->imageLabel->setPixmap(pixmap.scaled(400, 300, Qt::KeepAspectRatio));
+    }
+}
+
